6ecdf76a0299f28893c2b49319528324b65ec1a7
jaimesanchezgalvis <jaimesanchezgalvis@gmail.com>
2021-02-18 21:28:18 +0100

    v2

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
+const submitBtn = document.querySelector('.submit');
+const chatArea = document.querySelector('.chatPage');
+const inputElm = document.querySelector('input');
 
 
-function textmessages() {
-    document.getElementById("chatPage--box__ChatUser").value =
-    document.getElementById("textmessages").value;
-}
+submitBtn.addEventListener('click', () => {
+    let userInput = inputElm.value;
+    console.log(userInput);
+    let temp = `<div class="chatBox">
+    <img src="/public/usuario.jpg" class="chatBox--avatar__incomemessage">
+    <span class="chatBox--incomemessage">${userInput}</span>
+    </div>`;
+    chatArea.insertAdjacentHTML("beforeend", temp);
+    inputElm.value = '';
+})
 
