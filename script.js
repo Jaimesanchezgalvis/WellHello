5c2cbf566ee17a5e8a7da8ef30f51c97f28458e4
jaimesanchezgalvis <jaimesanchezgalvis@gmail.com>
2021-02-19 01:53:55 +0100

    2.1

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+
+const popup = document.querySelector('.containerChat');
+const chatBtn = document.querySelector('.chat-btn');
+const chatBtn1 = document.querySelector('.chat-btn1');
+const chatBtn2 = document.querySelector('.chat-btn2');
 const submitBtn = document.querySelector('.submit');
 const chatArea = document.querySelector('.chatPage');
 const inputElm = document.querySelector('input');
 
 
+chatBtn.addEventListener('click', () => {
+    popup.classList.toggle('show');
+})
+chatBtn1.addEventListener('click', () => {
+    popup.classList.toggle('show');
+})
+chatBtn2.addEventListener('click', () => {
+    popup.classList.toggle('show');
+})
 submitBtn.addEventListener('click', () => {
     let userInput = inputElm.value;
     console.log(userInput);
@@ -13,4 +27,14 @@ submitBtn.addEventListener('click', () => {
     chatArea.insertAdjacentHTML("beforeend", temp);
     inputElm.value = '';
 })
+
+
+function changeColor(x) {
+    if (x.style.background == "rgb(253, 242, 214)") {
+        x.style.background = "#EDEBEC";
+    } else {
+        x.style.background = "#FDF2D6";
+    }
+    return false;
+}
 
